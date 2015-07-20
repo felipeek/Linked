80617be7369ed30cf6c49e6e0c6ccdbbf1a6fa9d
Hoshoyo <pveiga1991@hotmail.com>
2015-07-20 02:58:53 -0300

    added font

--- Linked/Display.cpp
+++ Linked/Display.cpp
@@ -98,6 +98,7 @@ void Display::MainLoop()
 void Display::render()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
 	game->render();
 	glutSwapBuffers();
 }
--- Linked/Game.cpp
+++ Linked/Game.cpp
@@ -202,6 +202,8 @@ void Game::update()
 void Game::input()
 {
 	playerMovement->inputPlayerMovement();
+	if (Input::keyStates['v'])
+		player->setMaximumHpBasis(player->getMaximumHpBasis()+1);
 
 #ifdef DEBUG
 	if (Input::keyStates['t'])
